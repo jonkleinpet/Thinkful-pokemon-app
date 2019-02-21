7cc80c698b08010b2924816b9be30b36146b411f
Jonathan Kleinpeter <joklein341@gmail.com>
2019-02-21 12:53:35 -0600

    add cors and helmet for fetch requests

--- server.js
+++ server.js
@@ -3,12 +3,17 @@
 require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
+const helmet = require('helmet');
 const pokedex = require('./pokedex');
 console.log(process.env.API_TOKEN);
 
 const app = express();
 
 app.use(morgan('dev'));
+app.use(helmet());
+app.use(cors());
+
 app.use((req, res, next) => {
   const apiToken = process.env.API_TOKEN;
   const authToken = req.get('Authorization');
